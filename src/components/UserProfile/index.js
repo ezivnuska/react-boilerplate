3e743f6b8f7721fbaf742d8fdff54573817c40ac
Eric Zivnuska <ezivnuska@gmail.com>
2019-05-16 04:03:43 -0700

    connected data to UserProfile

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -2,7 +2,7 @@ import React, { Fragment, PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { PROFILE_PAGE } from 'queries'
 import { Query } from 'react-apollo'
-import { Heading } from 'components'
+import { Heading, ProfileImage } from 'components'
 
 import './UserProfile.scss'
 
@@ -21,21 +21,20 @@ class UserProfile extends PureComponent {
 
           if (loading) return <div></div>
           if (error) return <div>Error</div>
-
-          const userInfo = data.profilePage
-          const {
-            bio,
-            firstname,
-            lastname,
-            profileImage
-          } = userInfo
-
+          
+          const { bio, firstname, lastname, profileImage, username } = data.profilePage
+          
           return (
             <Fragment>
               <Heading level={2}>
                 {username}
               </Heading>
-              <div dangerouslySetInnerHTML={{__html: data.profilePage.bio}}></div>
+              {username && <p>{username}</p>}
+              {firstname && lastname && <p>{firstname} {lastname}</p>}
+              {firstname && !lastname && <p>{firstname}</p>}
+              {lastname && !firstname && <p>{lastname}</p>}
+              {profileImage && <ProfileImage src={profileImage} />}
+              {bio && <div dangerouslySetInnerHTML={{__html: bio}}></div>}
             </Fragment>
           )
         }}

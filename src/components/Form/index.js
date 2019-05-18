0e3e5ebf1a6ae21b52950e5818e013442473bfff
Eric Zivnuska <ezivnuska@gmail.com>
2019-05-17 20:12:17 -0700

    removed FormWrapper component; slight formatting adjustments.

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import FormWrapper from './form/FormWrapper'
+import { Heading } from 'components'
 
 import './Form.scss'
 
-const Form = ({ children, onSubmit, ...props }) => (
+const Form = ({ children, error, onSubmit, title, ...props }) => (
   <form className='form' onSubmit={onSubmit}>
-    <FormWrapper {...props}>
-      {children}
-    </FormWrapper>
+    <div className='form-wrapper' {...props}>
+      {title && <Heading level={3}>{title}</Heading>}
+      {error && <div className={(error !== '' ? 'error-label' : '')}>
+        {error}
+      </div>}
+      {(children && children.length) && <ul>
+        {children.map((section, i) => (
+          <li key={i} className='form-section'>{section}</li>
+        ))}
+      </ul>}
+    </div>
   </form>
 )
 

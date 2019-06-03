722dafc1c13231cbaa2d50502bf635cd894b9381
Eric Zivnuska <ezivnuska@gmail.com>
2019-06-03 05:13:16 -0700

    moved config.json to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  siteURL: `http://localhost:${process.env.PORT}`,
+  assetURL: `http://localhost:${process.env.PORT}/assets`,
+  profileImagesURL: `http://localhost:${process.env.PORT}/assets/user-uploads/profile-images`,
+  environment: 'development'
+}

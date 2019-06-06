f5d94e9bf98e80f2da3e771b016299a876eb2133
Eric Zivnuska <ezivnuska@gmail.com>
2019-06-05 20:07:09 -0700

    changes to convig vars

--- config/index.js
+++ config/index.js
@@ -1,12 +1,16 @@
 const environment = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || '3000'
+const isDev = environment === 'development'
+
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
 console.log('environment:', environment)
-const isDev = environment === 'development'
+
+const siteURL = isDev ? `http://localhost:${port}` : '/'
+
 const assetPath = 'assets'
+const assetURL = isDev ? `${siteURL}/${assetPath}` : `/${assetPath}`
+
 const profileImagesPath = 'user-uploads/profile-images'
-const siteURL = isDev ? `http://localhost:${port}` : 'http://ezivnuska.herokuapp.com'
-const assetURL = isDev ? `${siteURL}/${assetPath}` : assetPath
 const profileImagesURL = `${assetURL}/${profileImagesPath}`
 
 export default {

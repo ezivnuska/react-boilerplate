191ad5756636a28d2a6983d528602b8506fff783
Eric Zivnuska <ezivnuska@gmail.com>
2019-06-26 21:44:50 -0700

    added model Bond

---
+++ src/models/Bond/index.js
@@ -0,0 +1,31 @@
+import { model, Schema } from 'mongoose'
+
+const BondSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  responder: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  confirmed: {
+    type: Boolean,
+    default: false,
+  },
+  declined: {
+    type: Boolean,
+    default: false,
+  },
+  cancelled: {
+    type: Boolean,
+    default: false,
+  },
+},
+{
+  timestamps: true
+})
+
+export default model('Bond', BondSchema)

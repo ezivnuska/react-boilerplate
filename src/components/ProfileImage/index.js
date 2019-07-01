60ad88358a4eded5feab59cbc405f9f259c5bc32
Eric Zivnuska <ezivnuska@gmail.com>
2019-07-01 02:21:46 -0700

    added new graphics. default-avatar.png default-avatar-small.png

--- src/components/ProfileImage/index.js
+++ src/components/ProfileImage/index.js
@@ -9,7 +9,7 @@ const ProfileImage = ({ size, src }) => {
   const small = size <= 50
   const url = src
     ? `${webConfig.profileImagesURL}/${webConfig.profileImagesPath}/${small ? 'small/' : ''}${src}`
-    : `/${webConfig.assetPath}/graphics/default-avatar-small.png`
+    : `/${webConfig.assetPath}/graphics/default-avatar${small ? '-small' : ''}.png`
     
   return (
     <img

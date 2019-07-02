a2f81fbe23c45a92eb554d6f6e987b297d596a99
Eric Zivnuska <ezivnuska@gmail.com>
2019-07-01 18:21:45 -0700

    added Memory model

---
+++ src/models/Memory/index.js
@@ -0,0 +1,48 @@
+import { model, Schema } from 'mongoose'
+
+const MemorySchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    day: {
+        type: Number,
+        required: true
+    },
+    month: {
+        type: Number,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: false
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    shared: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    images: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Image'
+    }],
+    // comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+},
+{
+    timestamps: true
+})
+
+MemorySchema.pre('save', next => {
+    this.title = this.title || 'Untitled'
+    next()
+})
+
+export default model('Memory', MemorySchema)
